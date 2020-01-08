4cbb92826eb9acd102dc6fa7b4cedcb0563402b9
bitcolon <bitcolonofficial@gmail.com>
2020-01-08 22:17:15 +0530

    included requests in main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import requests
+
+r = requests.get('https://api.github.com/user', auth=('factober', '@darshan1'))
+print(r.status_code)
+print(r.headers['content-type'])
+print(r.encoding)
+print(r.text)
+print(r.json())
 print("Content Aggregator by Factober")
